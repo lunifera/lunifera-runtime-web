4865ec235fbaa0ad57f926978c78f9d363950d0f
jodoming <JoDominguez@compex-commerce.com>
2013-03-07 15:57:50 +0100

    New.

---
+++ org.lunifera.runtime.web.ecview.presentation.vaadin/src/org/lunifera/runtime/web/ecview/presentation/vaadin/internal/ButtonPresentation.java
@@ -0,0 +1,184 @@
+/**
+ * Copyright (c) 2013 COMPEX Systemhaus GmbH Heidelberg. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Jose C. Dominguez - initial API and implementation
+ */
+package org.lunifera.runtime.web.ecview.presentation.vaadin.internal;
+
+import org.eclipse.emf.ecp.ecview.common.editpart.IElementEditpart;
+import org.eclipse.emf.ecp.ecview.extension.model.extension.YButton;
+import org.eclipse.emf.ecp.ecview.ui.core.editparts.extension.IButtonEditpart;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.CssLayout;
+
+/**
+ * This presenter is responsible to render a text field on the given layout.
+ */
+public class ButtonPresentation extends
+		AbstractVaadinWidgetPresenter<Component> {
+
+	private final ModelAccess modelAccess;
+	private CssLayout componentBase;
+	private Button button;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param editpart
+	 *            The editpart of that presenter
+	 */
+	public ButtonPresentation(IElementEditpart editpart) {
+		super((IButtonEditpart) editpart);
+		this.modelAccess = new ModelAccess((YButton) editpart.getModel());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Component createWidget(Object parent) {
+		if (componentBase == null) {
+			componentBase = new CssLayout();
+			componentBase.addStyleName(CSS_CLASS__CONTROL_BASE);
+			if (modelAccess.isCssIdValid()) {
+				componentBase.setId(modelAccess.getCssID());
+			} else {
+				componentBase.setId(getEditpart().getId());
+			}
+
+			button = new Button();
+			button.addStyleName(CSS_CLASS__CONTROL);
+			button.setSizeFull();
+
+			// creates the binding for the field
+			createBindings(modelAccess.yButton, button);
+
+			componentBase.addComponent(button);
+
+			if (modelAccess.isCssClassValid()) {
+				button.addStyleName(modelAccess.getCssClass());
+			}
+
+			if (modelAccess.isLabelValid()) {
+				button.setCaption(modelAccess.getLabel());
+			}
+		}
+		return componentBase;
+	}
+
+	/**
+	 * Creates the bindings for the given values.
+	 * 
+	 * @param yButton
+	 * @param button
+	 */
+	protected void createBindings(YButton yButton, Button button) {
+		super.createBindings(yButton, button);
+	}
+
+	@Override
+	public Component getWidget() {
+		return componentBase;
+	}
+
+	@Override
+	public boolean isRendered() {
+		return componentBase != null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void unrender() {
+		if (componentBase != null) {
+			ComponentContainer parent = ((ComponentContainer) componentBase
+					.getParent());
+			if (parent != null) {
+				parent.removeComponent(componentBase);
+			}
+			componentBase = null;
+			button = null;
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void internalDispose() {
+		// unrender the ui component
+		unrender();
+	}
+
+	/**
+	 * A helper class.
+	 */
+	private static class ModelAccess {
+		private final YButton yButton;
+
+		public ModelAccess(YButton yButton) {
+			super();
+			this.yButton = yButton;
+		}
+
+		/**
+		 * @return
+		 * @see org.eclipse.emf.ecp.ecview.ui.core.model.core.YCssAble#getCssClass()
+		 */
+		public String getCssClass() {
+			return yButton.getCssClass();
+		}
+
+		/**
+		 * Returns true, if the css class is not null and not empty.
+		 * 
+		 * @return
+		 */
+		public boolean isCssClassValid() {
+			return getCssClass() != null && !getCssClass().equals("");
+		}
+
+		/**
+		 * @return
+		 * @see org.eclipse.emf.ecp.ecview.ui.core.model.core.YCssAble#getCssID()
+		 */
+		public String getCssID() {
+			return yButton.getCssID();
+		}
+
+		/**
+		 * Returns true, if the css id is not null and not empty.
+		 * 
+		 * @return
+		 */
+		public boolean isCssIdValid() {
+			return getCssID() != null && !getCssID().equals("");
+		}
+
+		/**
+		 * Returns true, if the label is valid.
+		 * 
+		 * @return
+		 */
+		public boolean isLabelValid() {
+			return yButton.getDatadescription() != null
+					&& yButton.getDatadescription().getLabel() != null;
+		}
+
+		/**
+		 * Returns the label.
+		 * 
+		 * @return
+		 */
+		public String getLabel() {
+			return yButton.getDatadescription().getLabel();
+		}
+	}
+}
