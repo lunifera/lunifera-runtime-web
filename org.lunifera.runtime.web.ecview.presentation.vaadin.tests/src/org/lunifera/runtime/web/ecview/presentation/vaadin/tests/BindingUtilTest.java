04d3c601f5e4fb460bc1b30c3ca5eea4d04fff1f
fpirchner <florian.pirchner@gmail.com>
2014-04-05 03:36:21 +0200

    Added Lifecycle support

---
+++ org.lunifera.runtime.web.ecview.presentation.vaadin.tests/src/org/lunifera/runtime/web/ecview/presentation/vaadin/tests/BindingUtilTest.java
@@ -0,0 +1,13 @@
+package org.lunifera.runtime.web.ecview.presentation.vaadin.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BindingUtilTest {
+
+	@Test
+	public void test() {
+		Assert.fail();
+	}
+
+}
