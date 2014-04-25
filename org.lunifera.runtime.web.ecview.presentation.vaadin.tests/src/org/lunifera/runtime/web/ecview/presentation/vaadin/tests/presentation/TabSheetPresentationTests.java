485e8f38653e30ae73fa95660b73c7525029a768
Florian Pirchner <florian.pirchner@gmail.com>
2014-04-25 12:59:42 +0200

    TabSheetTests

---
+++ org.lunifera.runtime.web.ecview.presentation.vaadin.tests/src/org/lunifera/runtime/web/ecview/presentation/vaadin/tests/presentation/TabSheetPresentationTests.java
@@ -0,0 +1,13 @@
+package org.lunifera.runtime.web.ecview.presentation.vaadin.tests.presentation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TabSheetPresentationTests {
+
+	@Test
+	public void test() {
+		Assert.fail("Implement");
+	}
+
+}
